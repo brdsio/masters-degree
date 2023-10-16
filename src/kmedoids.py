9bc0dd45641164a496d5f8b1dc5a500bea4e92eb
Flavio Duarte <flaviogabriel@gmail.com>
2023-10-15 22:04:18 -0300

    scenario 4 and 5

--- src/kmedoids.py
+++ src/kmedoids.py
@@ -334,3 +334,355 @@ class KMedoidScenario3(object):
             pesos_rebals.append(pesos)
 
         self.pesos_rebals = pd.concat(pesos_rebals)
+
+
+class KMedoidScenario4(object):
+    def __init__(self, precos, tickers, debug=True):
+        self.precos = precos
+        self.tickers = tickers
+
+        self._calcular_meses()
+        self.debug = debug
+
+    def _calcular_meses(self):
+        datas = pd.DataFrame(self.precos.index)
+        datas["amanha"] = datas.date.shift(-1)
+        self.meses = pd.DataFrame(
+            datas[datas.date.dt.month != datas.amanha.dt.month].date
+        )
+
+    def fit(self, k=3, window=3, max_iter=10):
+        self.k = k
+        grupos = {}
+        precos_mes = {}
+
+        for i in range(window, self.meses.shape[0]):
+            filtro_datas = self.meses[i - window : i]
+            if self.debug == True:
+                print("Datas ", filtro_datas.date.min(), filtro_datas.date.max())
+            precos_filtro = self.precos.loc[
+                filtro_datas.date.min() : filtro_datas.date.max()
+            ]
+
+            tickers_filtro = mi.ibov_constituents[
+                datetime.date(
+                    filtro_datas.date.max().year, filtro_datas.date.max().month, 1
+                )
+            ]
+            precos_filtro = precos_filtro[tickers_filtro]
+
+            if filtro_datas.date.max().date() == datetime.date(2017, 10, 31):
+                precos_filtro = precos_filtro.drop(["CPFE3"], axis=1)
+
+            returns = precos_filtro.pct_change()[1:].dropna(axis=1)
+            returns2 = precos_filtro.pct_change()
+
+            cov = returns.cov()
+            corr = returns.corr()
+
+            dist = correlDist(corr)
+
+            fuzzy = self._calcular_fuzzy_kmedoids(k, dist, max_iter, returns, returns2)
+            grupos[filtro_datas.date.max().date()] = fuzzy
+            precos_mes[filtro_datas.date.max().date()] = precos_filtro
+
+        self.precos_mes = precos_mes
+        self.grupos_fuzzy = grupos
+
+    def _calcular_fuzzy_kmedoids(self, k, matrix, max_iter, returns, returns2):
+        centroids = []
+
+        dt = pd.DataFrame(matrix.sum(axis=1), columns=["distancia"])
+
+        dt["quantile"] = pd.qcut(dt.distancia, q=k, labels=False)
+        dt = dt.sort_values(by="distancia")
+
+        for q in dt["quantile"].unique():
+            pos = find_nearest(dt.distancia, dt[dt["quantile"] == q].distancia.mean())
+            centroids.append(dt.index[pos])
+
+        centroids = random.sample(list(matrix.columns), k)
+
+        pcs = (1 + returns).cumprod().fillna(1)
+        centroids = (
+            (returns.std() * np.sqrt(252)).sort_values()[:k].index.tolist()
+        )  # genial
+
+        centroids_swap = copy.copy(centroids)
+
+        for i in range(max_iter):
+            C = self._calcular_grupos(matrix, centroids)
+
+            for centroid, data in C.items():
+                # sum_distance = np.sum(matrix.loc[data,data], axis=1)
+                # closest_medoid = np.argmin(sum_distance)
+
+                # new_centroid = sum_distance.index[closest_medoid]
+                # centroids_swap[centroids_swap.index(centroid)] = new_centroid
+                new_centroid = np.argmin(np.sum(matrix.loc[data, data], axis=1))
+                centroids_swap[centroids_swap.index(centroid)] = new_centroid
+
+            if np.array_equal(centroids, centroids_swap):
+                if self.debug:
+                    print("Convergiu", i)
+                break
+
+            centroids = copy.copy(centroids_swap)
+        else:
+            print("Halted")
+
+        grupos_fuzzy = self._calcular_fuzzy(C, matrix, returns2)
+        return grupos_fuzzy
+
+    def _calcular_grupos(self, matrix, centroids):
+        if self.debug:
+            print("Calculando grupos ", centroids)
+
+        C = {}
+        clusters = matrix[centroids].idxmin(axis=1)
+        for centroid in centroids:
+            C[centroid] = clusters[clusters == centroid].index.values
+        return C
+
+    def _calcular_fuzzy(self, C, dist, returns, m=2):
+        preco = (1 + returns).cumprod().fillna(1)
+        qtd_dias = len(preco)
+
+        retorno_periodo = (
+            preco[preco.index == preco.index.max()]
+            .stack()
+            .reset_index()
+            .rename(columns={0: "retorno"})
+        )
+
+        grupos_fuzzy = {}
+
+        grupos = list(C.keys())
+        for g in grupos:
+            # print(g)
+            dft = pd.DataFrame(C[g], columns=["ticker"])
+            dft = dft.merge(retorno_periodo, how="left", on="ticker")
+            dft["peso"] = dft.retorno / dft.retorno.sum()
+            grupos_fuzzy[g] = dft
+
+        return grupos_fuzzy
+
+    def pesos(self):
+        pesos_rebals = []
+        for date, values in self.grupos_fuzzy.items():
+            lista_pesos = []
+            grupos = values.keys()
+
+            for g in grupos:
+                pesos = values[g].set_index("ticker").peso
+                pesos = (pesos / (pesos.sum())) * 1 / self.k
+                pesos.name = "peso"
+                pesos = pesos.reset_index().rename(columns={"index": "ticker"})
+                lista_pesos.append(pesos)
+            pesos = pd.concat(lista_pesos)
+            pesos = pd.merge(pesos, self.tickers, how="left", on="ticker")
+            pesos["date"] = date
+
+            pesos_rebals.append(pesos)
+
+        self.pesos_rebals = pd.concat(pesos_rebals)
+
+
+class KMedoidScenario5(object):
+    def __init__(self, precos, tickers, debug=True):
+        self.precos = precos
+        self.tickers = tickers
+
+        self._calcular_meses()
+        self.debug = debug
+
+    def _calcular_meses(self):
+        datas = pd.DataFrame(self.precos.index)
+        datas["amanha"] = datas.date.shift(-1)
+        self.meses = pd.DataFrame(
+            datas[datas.date.dt.month != datas.amanha.dt.month].date
+        )
+        # self.meses = self.meses.head(14)
+
+    def fit(self, k=3, window=3, max_iter=10):
+        self.k = k
+        grupos = {}
+        precos_mes = {}
+
+        for i in range(window, self.meses.shape[0]):
+            filtro_datas = self.meses[i - window : i]
+            if self.debug == True:
+                print("Datas ", filtro_datas.date.min(), filtro_datas.date.max())
+            precos_filtro = self.precos.loc[
+                filtro_datas.date.min() : filtro_datas.date.max()
+            ]
+
+            tickers_filtro = mi.ibov_constituents[
+                datetime.date(
+                    filtro_datas.date.max().year, filtro_datas.date.max().month, 1
+                )
+            ]
+            precos_filtro = precos_filtro[tickers_filtro]
+
+            if filtro_datas.date.max().date() == datetime.date(2017, 10, 31):
+                precos_filtro = precos_filtro.drop(["CPFE3"], axis=1)
+
+            returns = precos_filtro.pct_change()[1:].dropna(axis=1)
+            returns2 = precos_filtro.pct_change()
+
+            cov = returns.cov()
+            corr = returns.corr()
+
+            dist = correlDist(corr)
+
+            fuzzy = self._calcular_fuzzy_kmedoids(k, dist, max_iter, returns, returns2)
+            grupos[filtro_datas.date.max().date()] = fuzzy
+            precos_mes[filtro_datas.date.max().date()] = precos_filtro
+
+        self.precos_mes = precos_mes
+        self.grupos_fuzzy = grupos
+
+    def _calcular_fuzzy_kmedoids(self, k, matrix, max_iter, returns, returns2):
+        centroids = []
+
+        dt = pd.DataFrame(matrix.sum(axis=1), columns=["distancia"])
+
+        dt["quantile"] = pd.qcut(dt.distancia, q=k, labels=False)
+        dt = dt.sort_values(by="distancia")
+
+        for q in dt["quantile"].unique():
+            pos = find_nearest(dt.distancia, dt[dt["quantile"] == q].distancia.mean())
+            centroids.append(dt.index[pos])
+
+        centroids = random.sample(list(matrix.columns), k)
+
+        pcs = (1 + returns).cumprod().fillna(1)
+        centroids = (
+            (returns.std() * np.sqrt(252)).sort_values()[:k].index.tolist()
+        )  # genial
+
+        centroids_swap = copy.copy(centroids)
+
+        for i in range(max_iter):
+            C = self._calcular_grupos(matrix, centroids)
+
+            for centroid, data in C.items():
+                new_centroid = np.argmin(np.sum(matrix.loc[data, data], axis=1))
+                centroids_swap[centroids_swap.index(centroid)] = new_centroid
+
+            if np.array_equal(centroids, centroids_swap):
+                if self.debug:
+                    print("Convergiu", i)
+                break
+
+            centroids = copy.copy(centroids_swap)
+        else:
+            print("Halted")
+
+        grupos_fuzzy = self._calcular_fuzzy(C, matrix, returns2)
+        return grupos_fuzzy
+
+    def _calcular_grupos(self, matrix, centroids):
+        if self.debug:
+            print("Calculando grupos ", centroids)
+
+        C = {}
+        clusters = matrix[centroids].idxmin(axis=1)
+        for centroid in centroids:
+            C[centroid] = clusters[clusters == centroid].index.values
+        return C
+
+    def _calcular_fuzzy(self, C, dist, returns, m=2):
+        # print(returns)
+        preco = (1 + returns).cumprod().fillna(1)
+        # print(preco['ABEV3'])
+        qtd_dias = len(preco)
+        # retorno_precos = preco.
+
+        # print(preco)
+        retorno_periodo = (
+            preco[preco.index == preco.index.max()]
+            .stack()
+            .reset_index()
+            .rename(columns={0: "retorno"})
+        )
+
+        grupos_fuzzy = {}
+
+        grupos = list(C.keys())
+        for g in grupos:
+            # print(g)
+            dft = pd.DataFrame(C[g], columns=["ticker"])
+            dft = dft.merge(retorno_periodo, how="left", on="ticker")
+            dft["peso"] = dft.retorno / dft.retorno.sum()
+            # print(dft)
+            # print("---")
+            # print("-----")
+            # papeis_grupo = list(C[g])
+
+            # distancias = dist.loc[papeis_grupo, grupos]
+            # distancias.loc[g] = (distancias.loc[g].index == g) + 0
+
+            # fkdist = (1 / distancias).replace([pd.np.inf], [0]).pow(exp)
+            # fkdist = fkdist.divide(fkdist.sum(axis=1), axis='rows')
+
+            # print(fkdist)
+            grupos_fuzzy[g] = dft
+
+        return grupos_fuzzy
+
+    def pesos(self):
+        pesos_rebals = []
+        for date, values in self.grupos_fuzzy.items():
+            # print(date)
+
+            qtd_dias = len(self.precos_mes[date])
+
+            retorno_periodo = self.precos_mes[date].pct_change(qtd_dias - 1)
+            retorno_periodo = (
+                retorno_periodo.stack()
+                .reset_index()
+                .rename(columns={0: "retorno_periodo"})
+            )
+            # print(values)
+            grupos = values.keys()
+            # print(grupos)
+            retorno_grupos = {}
+            for g in grupos:
+                # print(values)
+                frame_temp = pd.DataFrame(values[g])
+                # print(frame_temp)
+                frame_temp = frame_temp.merge(retorno_periodo, how="left", on="ticker")
+                frame_temp["valor_inicial"] = 1.0
+                frame_temp["valor_final"] = frame_temp["valor_inicial"] * (
+                    1 + frame_temp["retorno_periodo"]
+                )
+
+                retorno_grupo = (
+                    frame_temp["valor_final"].sum() / frame_temp["valor_inicial"].sum()
+                )
+                retorno_grupos[g] = retorno_grupo
+
+            # print(retorno_grupos)
+
+            # print(values)
+            lista_pesos = []
+            for g in grupos:
+                soma_retornos_grupos = sum(retorno_grupos.values())
+
+                proporcao_alocacao_grupo = retorno_grupos[g] / soma_retornos_grupos
+
+                # pesos = values[g].peso
+                # pesos = (pesos / (pesos.sum())) * proporcao_alocacao_grupo
+                # pesos.name = 'peso'
+                # pesos = pesos.reset_index().rename(columns={'index': 'ticker'})
+                pesos = values[g][["ticker", "peso"]].copy()
+                pesos["peso"] = pesos["peso"] * proporcao_alocacao_grupo
+                lista_pesos.append(pesos)
+
+            pesos = pd.concat(lista_pesos)
+            pesos = pd.merge(pesos, self.tickers, how="left", on="ticker")
+            pesos["date"] = date
+            pesos_rebals.append(pesos)
+
+        self.pesos_rebals = pd.concat(pesos_rebals)
---
+++ src/scenario5.py
@@ -0,0 +1,108 @@
+from typing import List, Dict
+import datetime
+from decimal import Decimal
+import pandas as pd
+import numpy as np
+
+
+from src.kmedoids import KMedoidScenario5
+from src.backtest import calculate_cumret, calculate_sharpe_ratio
+from src.config import get_data
+
+
+def main():
+    (
+        cdi,
+        ibov,
+        tickers,
+        prices,
+        prices_pivot,
+        prices_trading,
+        date_begin,
+        date_end,
+    ) = get_data()
+    """
+    Main function to run k-medoids and backtest.
+
+    Returns:
+    - results (List[Dict[str, float]]): List of dictionaries containing backtest results.
+    """
+    # Load financial data using the get_data function
+    (
+        cdi,
+        ibov,
+        tickers,
+        prices,
+        prices_pivot,
+        prices_trading,
+        date_begin,
+        date_end,
+    ) = get_data()
+
+    # Initialize an empty list to store the results
+    results = []
+
+    # Iterate through different values of 'k' (clusters) for k-medoids
+    for k in range(3, 21):
+        # Initialize and fit the k-medoids model
+        km = KMedoidScenario5(
+            prices_pivot[prices_pivot.index >= datetime.datetime(2007, 12, 30)],
+            tickers,
+            debug=False,
+        )
+        km.fit(k=k, window=13)
+        km.pesos()
+
+        # Extract and preprocess the allocation data within the specified date range
+        pesos = km.pesos_rebals[
+            (km.pesos_rebals.date >= date_begin) & (km.pesos_rebals.date <= date_end)
+        ].copy()
+        pesos["date"] = pd.to_datetime(pesos["date"])
+
+        # Calculate cumulative returns and turnover
+        cumret, turnover = calculate_cumret(
+            pesos.pivot_table(index="date", columns="ticker", values="peso").fillna(0),
+            prices_pivot,
+            prices_trading,
+        )
+
+        # Calculate various performance metrics
+        mean_turnover = turnover[1:].mean() * 100
+        cumret = cumret.dropna().loc[:date_end]
+
+        ret = cumret.tail(1).sub(1).mul(100).round(1).values[0][0]
+
+        volatility = (
+            cumret.dropna()
+            .loc[:date_end]
+            .pct_change()
+            .std()
+            .mul(np.sqrt(252))
+            .mul(100)
+            .round(1)
+            .values[0]
+        )
+        max_drawdown = (
+            cumret.div(cumret.expanding().max())
+            .sub(1)
+            .mul(100)
+            .min()
+            .round(1)
+            .values[0]
+        )
+        sharpe = calculate_sharpe_ratio(cumret, cdi)
+
+        # Store the results in a dictionary
+        dict_result = {
+            "k": k,
+            "return": round(Decimal(ret), 1),
+            "volatility": round(Decimal(volatility), 1),
+            "turnover": round(Decimal(mean_turnover), 1),
+            "drawdown": round(Decimal(max_drawdown), 1),
+            "sharpe": round(Decimal(sharpe), 1),
+        }
+
+        # Append the results dictionary to the results list
+        results.append(dict_result)
+
+    return results
